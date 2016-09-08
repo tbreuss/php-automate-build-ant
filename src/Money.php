a656ec6d136b3cb86cf3bff4e7d5e00f2edddfbd
Thomas Breuss <thomasbreuss@gmx.ch>
2016-09-08 22:07:39 +0200

    Added UnitTest example and composer.lock

---
+++ src/Money.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Money
+{
+    private $amount;
+
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function negate()
+    {
+        return new Money(-1 * $this->amount);
+    }
+
+}
